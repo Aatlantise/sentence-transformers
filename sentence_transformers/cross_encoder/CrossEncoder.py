efefb68bb0dd4bf4595060c94ea38ada95b74946
Junghyun Min <minj@wra.net>
2021-03-26 11:43:53 +0900

    Update CrossEncoder.py
    Indent block fix

--- sentence_transformers/cross_encoder/CrossEncoder.py
+++ sentence_transformers/cross_encoder/CrossEncoder.py
@@ -160,7 +160,7 @@ class CrossEncoder():
             scheduler = SentenceTransformer._get_scheduler(optimizer, scheduler=scheduler, warmup_steps=warmup_steps, t_total=num_train_steps)
 
         if loss_fct is None:
-        loss_fct = nn.BCEWithLogitsLoss() if self.config.num_labels == 1 else nn.CrossEntropyLoss()
+            loss_fct = nn.BCEWithLogitsLoss() if self.config.num_labels == 1 else nn.CrossEntropyLoss()
 
 
         skip_scheduler = False
