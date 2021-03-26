2cd2830edf2890302d432d90f8da100a0c126d68
Junghyun Min <jmin10@jhu.edu>
2021-03-26 11:06:21 +0900

    Update train_ssce.py
    Bug fix

--- examples/semi-supervised-crossencoder/train_ssce.py
+++ examples/semi-supervised-crossencoder/train_ssce.py
@@ -1,13 +1,13 @@
 from sentence_transformers import CrossEncoder
 from sentence_transformers.readers import InputExample
-from sentence_transformers.cross_encoder.evaluation import CEBinaryAccuracyEvaluator
+from sentence_transformers.cross_encoder.evaluation import CEBinaryClassificationEvaluator
 from torch.utils.data import DataLoader
 
 
 roberta = CrossEncoder('cross-encoder/stsb-roberta-base', num_labels = 1)
 tokens = ["<e1>", "<e2>"]
 roberta.tokenizer.add_tokens(tokens, special_tokens=True)
-roberta.resize_token_embeddings(len(roberta.tokenizer))
+roberta.model.resize_token_embeddings(len(roberta.tokenizer))
 
 with open('fewrel_tag/pairwise_labeled_train.tsv','r') as r:
     labeled_data = r.readlines()
@@ -44,10 +44,11 @@ for line in test_data:
 	test_labels.append(new_entry[1])
 
 train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=16)
-evaluator = CEBinaryAccuracyEvaluator(test_sentence_pairs, test_labels, write_csv = True)
+evaluator = CEBinaryClassificationEvaluator(test_sentence_pairs, test_labels, write_csv = True)
 
 roberta.fit(train_dataloader = train_dataloader,
-			evaluator = evaluator,
-			epochs = 5,
-			output_path = './ssce_save/'
-			)
+	evaluator = evaluator,
+	epochs = 5,
+	loss_fct = nn.BCEWithLogitsLoss(),
+	output_path = './ssce_save/'
+	)
