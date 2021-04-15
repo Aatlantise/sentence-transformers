e3502e64a5d8d9438f4f97fce20b0eff4453b630
Junghyun Min <jmin10@jhu.edu>
2021-04-15 10:36:31 +0900

    Update train_fsce.py
    Implement OOD eval

--- examples/semi-supervised-crossencoder/train_fsce.py
+++ examples/semi-supervised-crossencoder/train_fsce.py
@@ -53,3 +53,27 @@ roberta.fit(train_dataloader = train_dataloader,
 	loss_fct = nn.BCEWithLogitsLoss(),
 	output_path = './ssce_save/fsce/'
 	)
+
+with open('datasets/editor_standalone/processed_editor_eval_tag.tsv', 'r') as r:
+	OOD_data = r.readlines()
+
+OOD_sentence_pairs = []
+OOD_labels = []
+for line in OOD_data:
+	pair = line.strip('\n').split('\t')
+	new_entry = []
+	try:
+		new_entry.append([pair[0], pair[1]])
+	except:
+		continue
+	try:
+		new_entry.append(int(pair[2]))
+	except:
+		continue
+	OOD_sentence_pairs.append(new_entry[0])
+	OOD_labels.append(new_entry[1])
+
+OOD_evaluator = CEBinaryClassificationEvaluator(OOD_sentence_pairs, OOD_labels)
+
+OOD_evaluator(model = roberta,
+    output_path = './ssce_save/fsce/')
