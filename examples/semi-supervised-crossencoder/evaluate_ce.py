0c6175568f57e4063bbad31e70a3f712e1c38890
Junghyun Min <jmin10@jhu.edu>
2021-04-23 10:16:02 +0900

    Update evaluate_ce.py
    Add hyperparameters to evaluator

--- examples/semi-supervised-crossencoder/evaluate_ce.py
+++ examples/semi-supervised-crossencoder/evaluate_ce.py
@@ -9,7 +9,7 @@ from torch import nn
 sys.path.append(os.path.abspath('lib/'))
 
 
-def evaluate_ce(num_labels, dataset_name, evalset_name):
+def evaluate_ce(num_labels, dataset_name, evalset_name, evaluator, threshold):
     model = CrossEncoder('ssce_save/fsce/' + dataset_name, num_labels = num_labels)
 
     with open(evalset_name, 'r') as r:
@@ -31,7 +31,10 @@ def evaluate_ce(num_labels, dataset_name, evalset_name):
         OOD_sentence_pairs.append(new_entry[0])
         OOD_labels.append(new_entry[1])
 
-    OOD_evaluator = CEBinaryAccuracyEvaluator(OOD_sentence_pairs, OOD_labels)
+    if evaluator == 'accuracy':
+        OOD_evaluator = CEBinaryAccuracyEvaluator(OOD_sentence_pairs, OOD_labels, threshold = threshold)
+    elif evaluator == 'classification':
+         OOD_evaluator = CEBinaryClassificationEvaluator(OOD_sentence_pairs, OOD_labels)
 
     OOD_evaluator(model = model,
         output_path = 'ssce_save/fsce/' + dataset_name)
@@ -41,11 +44,15 @@ if __name__=='__main__':
     parser.add_argument("--dataset_name", type = str, default = 'augmented_ko_re_tag')
     parser.add_argument("--num_labels", type = int, default = 2)
     parser.add_argument("--evalset_name", type = str, default = 'datasets/editor_standalone/pairwise_OOD.tsv')
+    parser.add_argument("--evaluator", type = str, default = 'accuracy')
+    parser.add_argument("--num_labels", type = float, default = 0.5)
 
     args = parser.parse_args()
 
     evaluate_ce(
         num_labels = args.num_labels,
         dataset_name = args.dataset_name,
-        evalset_name = args.evalset_name
+        evalset_name = args.evalset_name,
+        evaluator = args.evaluator,
+        threshold = args.threshold
     )
